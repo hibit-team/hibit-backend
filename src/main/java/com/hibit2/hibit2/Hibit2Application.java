a801a6df5b1e01dafddbba8af17773be0c58bf69
hyoung <ycat01@naver.com>
2023-04-05 00:39:35 +0900

    Branch Setting

---
+++ src/main/java/com/hibit2/hibit2/Hibit2Application.java
@@ -0,0 +1,14 @@
+package com.hibit2.hibit2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class Hibit2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hibit2Application.class, args);
+	}
+
+}
