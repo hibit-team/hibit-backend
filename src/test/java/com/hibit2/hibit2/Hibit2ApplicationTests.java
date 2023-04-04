fba9509ce24d571ff44fb85bb8dcec404af7ac8d
hyoung <ycat01@naver.com>
2023-04-05 00:39:35 +0900

    Setting

---
+++ src/test/java/com/hibit2/hibit2/Hibit2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hibit2.hibit2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hibit2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
