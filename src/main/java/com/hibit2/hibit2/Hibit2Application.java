a0acbf7fb92c4949f9f44d2801bb72d615dba049
hyoung <ycat01@naver.com>
2023-05-04 01:12:14 +0900

    test : s3

--- src/main/java/com/hibit2/hibit2/Hibit2Application.java
+++ src/main/java/com/hibit2/hibit2/Hibit2Application.java
@@ -3,8 +3,11 @@ package com.hibit2.hibit2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+//@SpringBootApplication(exclude ={ContextStackAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = {ContextStackAutoConfiguration.class, SecurityAutoConfiguration.class,})
 public class Hibit2Application {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/hibit2/hibit2/controller/FileUploadController.java
@@ -0,0 +1,21 @@
+package com.hibit2.hibit2.controller;
+
+import com.hibit2.hibit2.service.S3Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+public class FileUploadController {
+    @Autowired
+    private S3Service s3Service;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
+        String fileUrl = s3Service.uploadFile(file);
+        return ResponseEntity.ok(fileUrl);
+    }
+}
