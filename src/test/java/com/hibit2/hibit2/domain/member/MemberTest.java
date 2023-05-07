e30dfd18ab8bfb25992aa7f4a8b7bc8183c48125
junyong <jymoon1121@gmail.com>
2023-05-07 22:32:06 +0900

    fix: JUnit5에 맞는@Test 어노테이션 사용
    - import org.junit.jupiter.api.Test 라이브러리를 사용

--- src/test/java/com/hibit2/hibit2/domain/member/MemberTest.java
+++ src/test/java/com/hibit2/hibit2/domain/member/MemberTest.java
@@ -3,7 +3,7 @@ package com.hibit2.hibit2.domain.member;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
