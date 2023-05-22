5574494b93eb13cd9b946b7508ada06961ecd153
hyoung <ycat01@naver.com>
2023-05-22 16:50:57 +0900

    [CICD] github action test

--- src/main/java/com/hibit2/hibit2/controller/Sample.java
+++ src/main/java/com/hibit2/hibit2/controller/Sample.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Sample {
     @RequestMapping("/sample")
     public String greeting(){
-        return "sample!!";
+        return "sample!";
     }
 }
