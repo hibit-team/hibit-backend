9f7b32ec1f3d790461bab6a21d6f15f2fdddaf61
Junyong Moon <fancy.junyongmoon@gmail.com>
2023-06-01 18:33:35 +0900

    refactor: 닉네임 20글자로 수정

--- src/main/java/com/hibit2/hibit2/domain/member/domain/Member.java
+++ src/main/java/com/hibit2/hibit2/domain/member/domain/Member.java
@@ -21,7 +21,7 @@ import lombok.Getter;
 @Entity
 public class Member {
     private static final Pattern EMAIL_PATTERN = Pattern.compile("^[a-z0-9._-]+@[a-z]+[.]+[a-z]{2,3}$");
-    private static final int MAX_NICK_NAME_LENGTH = 10;
+    private static final int MAX_NICK_NAME_LENGTH = 20;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
