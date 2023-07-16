9f37aef4a65cddb4a7cefbb8d47b7b76be16ab2d
junyong <jymoon1121@gmail.com>
2023-07-16 22:08:07 +0900

    feat: ProfileController에서 PostMapping에 대한 @Operation 추가
    - swagger-ui api 동작에 대한 명세를 작성하기 위해 Controller 메서드에 설정

--- src/main/java/com/hibit2/hibit2/profile/controller/ProfileController.java
+++ src/main/java/com/hibit2/hibit2/profile/controller/ProfileController.java
@@ -14,6 +14,8 @@ import com.hibit2.hibit2.profile.dto.request.RegisterProfileRequest;
 import com.hibit2.hibit2.profile.dto.response.RegisterProfileResponse;
 import com.hibit2.hibit2.profile.service.ProfileService;
 
+import io.swagger.v3.oas.annotations.Operation;
+
 @RestController
 @RequestMapping("/profiles")
 public class ProfileController {
@@ -26,6 +28,7 @@ public class ProfileController {
 
     // 프로필 등록
     @PostMapping
+    @Operation(summary = "/profiles/1", description = "프로필 등록")
     public ResponseEntity<Void> registerProfile(@Valid @RequestBody RegisterProfileRequest registerProfileRequest) {
 
         RegisterProfileResponse profile = profileService.registerProfile(registerProfileRequest);
