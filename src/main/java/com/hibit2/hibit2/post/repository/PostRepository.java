470670a71f2f0cb99853c37ab756562f04f9924b
hyoung <ycat01@naver.com>
2023-07-18 19:09:48 +0900

    [FEAT] 이번주 리스트 수정

--- src/main/java/com/hibit2/hibit2/post/repository/PostRepository.java
+++ src/main/java/com/hibit2/hibit2/post/repository/PostRepository.java
@@ -16,8 +16,10 @@ public interface PostRepository extends JpaRepository<Post, Integer> {
     List<Post> findByStatusNot(char flag, Sort sort);
     Page<Post> findByStatusNot(char flag, Pageable pageable);
 
-    @Query("SELECT p FROM Post p JOIN p.dateTimeSlots dts WHERE p.status = :flag AND dts.date >= :startDate AND dts.date < :endDate")
-    Page<Post> findByDateTimeRange(@Param("flag") char flag, @Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate, Pageable pageable);
+
+    @Query("SELECT p FROM Post p JOIN p.dateTimeSlots dts WHERE (p.status <> :flag OR :flag IS NULL) AND dts.date >= :startDate AND dts.date < :endDate")
+    Page<Post> findByDateTimeRange(@Param("flag") Character flag, @Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate, Pageable pageable);
+
 
     Page<Post> findByStatusNotAndTitleContainingOrExhibitionContaining(
             char flag, String keyword, String keyword2, Pageable pageable);
