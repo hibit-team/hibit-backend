6f5e390e737538a4c37907e1f14aee187da926c0
Junyong Moon <fancy.junyongmoon@gmail.com>
2023-07-20 10:12:52 +0900

    feat: token발급시, AccessToken과 RefreshToken을 함께 발급하는 기능 생성

---
+++ src/main/java/com/hibit2/hibit2/auth/dto/AccessAndRefreshTokenResponse.java
@@ -0,0 +1,21 @@
+package com.hibit2.hibit2.auth.dto;
+
+public class AccessAndRefreshTokenResponse {
+
+    private final String accessToken;
+
+    private final String refreshToken;
+
+    public AccessAndRefreshTokenResponse(String accessToken, String refreshToeken) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToeken;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+}
