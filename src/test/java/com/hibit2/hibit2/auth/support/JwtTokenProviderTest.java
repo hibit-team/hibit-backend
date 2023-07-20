791e7cfa601fcc0e690ba63b97b47d4e99547583
Junyong Moon <fancy.junyongmoon@gmail.com>
2023-07-20 10:24:24 +0900

    refactor: 토큰 생성시 리프레시 토큰도 같이 생성

--- src/test/java/com/hibit2/hibit2/auth/support/JwtTokenProviderTest.java
+++ src/test/java/com/hibit2/hibit2/auth/support/JwtTokenProviderTest.java
@@ -4,62 +4,72 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.hibit2.hibit2.auth.exception.InvalidTokenException;
+import com.hibit2.hibit2.auth.service.TokenProvider;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class JwtTokenProviderTest {
     private static final String JWT_SECRET_KEY = "A".repeat(32); // Secret Key는 최소 32바이트 이상이어야함.
-    private static final int JWT_EXPIRE_LENGTH = 3600;
+    private static final int JWT_ACCESS_TOKEN_EXPIRE_LENGTH = 3600;
+    private static final int JWT_REFRESH_TOKEN_EXPIRE_LENGTH = 3600;
+    private static final String PAYLOAD = "payload";
 
-    private final JwtTokenProvider jwtTokenProvider = new JwtTokenProvider(JWT_SECRET_KEY, JWT_EXPIRE_LENGTH);
-
-    @DisplayName("JWT 토큰을 생성한다.")
+    private final JwtTokenProvider jwtTokenProvider = new JwtTokenProvider(JWT_SECRET_KEY,
+            JWT_ACCESS_TOKEN_EXPIRE_LENGTH, JWT_REFRESH_TOKEN_EXPIRE_LENGTH);
+    @DisplayName("엑세스 토큰을 생성한다.")
     @Test
-    void JWT_토큰을_생성한다() {
-        // given
-        String payload = "hello";
-
-        // when
-        String actual = jwtTokenProvider.createToken(payload);
+    void 엑세스_토큰을_생성한다() {
+        // given & when
+        String actual = jwtTokenProvider.createAccessToken(PAYLOAD);
 
         // then
         assertThat(actual.split("\\.")).hasSize(3);
     }
 
-    @DisplayName("JWT 토큰의 Payload를 가져온다.")
+    @DisplayName("리프레시 토큰을 생성한다.")
     @Test
-    void JWT_토큰의_Payload를_가져온다() {
+    void 리프레시_토큰을_생성한다() {
+        // given & when
+        String actual = jwtTokenProvider.createRefreshToken(PAYLOAD);
+
+        // then
+        assertThat(actual.split("\\.")).hasSize(3);
+    }
+
+    @DisplayName("토큰의 Payload를 가져온다.")
+    @Test
+    void 토큰의_Payload를_가져온다() {
         // given
-        String expected = "Hello";
-        String token = jwtTokenProvider.createToken(expected);
+        String token = jwtTokenProvider.createAccessToken(PAYLOAD);
 
         // when
         String actual = jwtTokenProvider.getPayload(token);
 
         // then
-        assertThat(actual).isEqualTo(expected);
+        assertThat(actual).isEqualTo(PAYLOAD);
     }
 
-    @DisplayName("validateToken 메서드는 만료된 토큰을 전달하면 예외를 던진다.")
+    @DisplayName("엑세스 토큰을 검증하여 만료된 경우 예외를 던진다.")
     @Test
-    void validateToken_메서드는_만료된_토큰을_전달하면_예외를_던진다() {
+    void 엑세스_토큰을_검증하여_만료된_경우_예외를_던진다() {
         // given
-        JwtTokenProvider expiredJwtTokenProvider = new JwtTokenProvider(JWT_SECRET_KEY, 0);
-        String expiredToken = expiredJwtTokenProvider.createToken("payload");
+        TokenProvider expiredJwtTokenProvider = new JwtTokenProvider(JWT_SECRET_KEY, 0, 0);
+        String expiredToken = expiredJwtTokenProvider.createAccessToken(PAYLOAD);
 
         // when & then
         assertThatThrownBy(() -> jwtTokenProvider.validateToken(expiredToken))
-            .isInstanceOf(InvalidTokenException.class);
+                .isInstanceOf(InvalidTokenException.class);
     }
 
-    @DisplayName("validateToken 메서드는 유효하지 않은 토큰을 전달하면 예외를 던진다.")
+    @DisplayName("리프레시 토큰을 검증하여 만료된 경우 예외를 던진다.")
     @Test
-    void validateToken_메서드는_유효하지_않은_토큰을_전달하면_예외를_던진다() {
+    void 리프레시_토큰을_검증하여_만료된_경우_예외를_던진다() {
         // given
-        String malformedToken = "malformed";
+        TokenProvider expiredJwtTokenProvider = new JwtTokenProvider(JWT_SECRET_KEY, 0, 0);
+        String expiredToken = expiredJwtTokenProvider.createRefreshToken(PAYLOAD);
 
         // when & then
-        assertThatThrownBy(() -> jwtTokenProvider.validateToken(malformedToken))
-            .isInstanceOf(InvalidTokenException.class);
+        assertThatThrownBy(() -> jwtTokenProvider.validateToken(expiredToken))
+                .isInstanceOf(InvalidTokenException.class);
     }
 }
