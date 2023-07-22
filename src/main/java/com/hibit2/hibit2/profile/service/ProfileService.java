ffe37b412909f4a0c9a6a7a867ab54fb747bb3ad
junyong <jymoon1121@gmail.com>
2023-07-22 23:09:22 +0900

    feat: 유저 자신의 프로필을 조회하는 기능 구현

--- src/main/java/com/hibit2/hibit2/profile/service/ProfileService.java
+++ src/main/java/com/hibit2/hibit2/profile/service/ProfileService.java
@@ -6,6 +6,8 @@ import org.springframework.transaction.annotation.Transactional;
 import com.hibit2.hibit2.profile.domain.Profile;
 import com.hibit2.hibit2.profile.dto.request.RegisterProfileRequest;
 import com.hibit2.hibit2.profile.dto.response.RegisterProfileResponse;
+import com.hibit2.hibit2.profile.dto.response.UserProfileResponse;
+import com.hibit2.hibit2.profile.exception.NotFoundProfileException;
 import com.hibit2.hibit2.profile.repository.ProfileRepository;
 
 
@@ -37,4 +39,9 @@ public class ProfileService {
         return new RegisterProfileResponse(saveProfile);
     }
 
+    public UserProfileResponse getProfile(int profileId) {
+        Profile profile = profileRepository.findById(profileId)
+            .orElseThrow(NotFoundProfileException::new);
+        return new UserProfileResponse(profile);
+    }
 }
