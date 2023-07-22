8c6b173d27878b69d24d3eb2974a1a5f2bc6328d
junyong <jymoon1121@gmail.com>
2023-07-22 23:10:56 +0900

    chore: Sample 클래스 위치 이동
    - hibit2 패키지 아래로 이동

--- src/main/java/com/hibit2/hibit2/Sample.java
+++ src/main/java/com/hibit2/hibit2/Sample.java
@@ -1,4 +1,4 @@
-package com.hibit2.hibit2.controller;
+package com.hibit2.hibit2;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
