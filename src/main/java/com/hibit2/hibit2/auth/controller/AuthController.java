d05d96ef31dae0b6edd52ffa8c4dd986d65d4efb
Junyong Moon <fancy.junyongmoon@gmail.com>
2023-07-30 10:23:16 +0900

    feat: 웹페이지 로딩 시, 토큰이 유효한지 검증

--- src/main/java/com/hibit2/hibit2/auth/controller/AuthController.java
+++ src/main/java/com/hibit2/hibit2/auth/controller/AuthController.java
@@ -1,40 +1,66 @@
 package com.hibit2.hibit2.auth.controller;
 
+import com.hibit2.hibit2.auth.dto.LoginMember;
+import com.hibit2.hibit2.auth.dto.OAuthMember;
+import com.hibit2.hibit2.auth.dto.request.TokenRenewalRequest;
+import com.hibit2.hibit2.auth.dto.response.AccessTokenResponse;
+import com.hibit2.hibit2.auth.dto.response.OAuthUriResponse;
+import com.hibit2.hibit2.auth.support.AuthenticationPrincipal;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.hibit2.hibit2.auth.dto.request.TokenRequest;
 import com.hibit2.hibit2.auth.dto.response.AccessAndRefreshTokenResponse;
+import com.hibit2.hibit2.auth.support.OAuthUri;
+import com.hibit2.hibit2.auth.support.OAuthClient;
 import com.hibit2.hibit2.auth.service.AuthService;
-import com.hibit2.hibit2.global.dto.CommonResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
 
 @RequestMapping("/api/auth")
 @RestController
 public class AuthController {
 
+    private final OAuthUri oAuthUri;
+    private final OAuthClient oAuthClient;
     private final AuthService authService;
 
-    public AuthController(final AuthService authService) {
+    public AuthController(final OAuthUri oAuthUri, final OAuthClient oAuthClient, final AuthService authService) {
+        this.oAuthUri = oAuthUri;
+        this.oAuthClient = oAuthClient;
         this.authService = authService;
     }
 
-    @GetMapping("/{oauthProvider}/link")
-    public ResponseEntity<CommonResponse<String>>
-    generateLink(@PathVariable final String oauthProvider) {
-        return ResponseEntity.ok(new CommonResponse<>
-            (authService.generateGoogleLink()));
+    @GetMapping("/{oauthProvider}/oauth-uri")
+    public ResponseEntity<OAuthUriResponse> generateLink(@PathVariable final String oauthProvider,
+                                                         @RequestParam final String redirectUri) {
+        OAuthUriResponse oAuthUriResponse = new OAuthUriResponse(oAuthUri.generate(redirectUri));
+        return ResponseEntity.ok(oAuthUriResponse);
     }
 
     @PostMapping("/{oauthProvider}/token")
-    public ResponseEntity<CommonResponse<AccessAndRefreshTokenResponse>>
-    generateToken(@PathVariable final String oauthProvider,
-        @RequestBody final TokenRequest tokenRequest) {
-        AccessAndRefreshTokenResponse accessAndRefreshTokenResponse = authService.generateAccessAndRefreshTokenWithCode(tokenRequest.getCode());
-        return ResponseEntity.ok(new CommonResponse<>(accessAndRefreshTokenResponse));
+    public ResponseEntity<AccessAndRefreshTokenResponse> generateAccessAndRefreshToken(
+            @PathVariable final String oauthProvider, @Valid @RequestBody final TokenRequest tokenRequest) {
+        OAuthMember oAuthMember = oAuthClient.getOAuthMember(tokenRequest.getCode(), tokenRequest.getRedirectUri());
+        AccessAndRefreshTokenResponse response = authService.generateAccessAndRefreshToken(oAuthMember);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/token/access")
+    public ResponseEntity<AccessTokenResponse> generateAccessToken(
+            @Valid @RequestBody final TokenRenewalRequest tokenRenewalRequest) {
+        AccessTokenResponse response = authService.generateAccessToken(tokenRenewalRequest);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/validate/token")
+    public ResponseEntity<Void> validateToken(@AuthenticationPrincipal final LoginMember loginMember) {
+        return ResponseEntity.ok().build();
     }
 }
