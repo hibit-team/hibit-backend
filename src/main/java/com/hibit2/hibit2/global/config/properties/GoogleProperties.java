f6ed8ef946a6c02971d399ecb38122865d155fa4
Junyong Moon <fancy.junyongmoon@gmail.com>
2023-07-30 18:59:10 +0900

    fix: @Value 어노테이션으로 application.yml에 있는 값 설정
    - 추가로 application-local.yml 파일을 application.yml로 이름 수정하니 에러 해결됨

--- src/main/java/com/hibit2/hibit2/global/config/properties/GoogleProperties.java
+++ src/main/java/com/hibit2/hibit2/global/config/properties/GoogleProperties.java
@@ -1,9 +1,8 @@
 package com.hibit2.hibit2.global.config.properties;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
-
-import java.util.List;
 
 @ConfigurationProperties("oauth.google")
 @ConstructorBinding
@@ -12,13 +11,17 @@ public class GoogleProperties {
     private final String clientSecret;
     private final String oAuthEndPoint;
     private final String responseType;
-    private final List<String> scopes;
+    private final String scopes;
     private final String tokenUri;
     private final String accessType;
 
-    public GoogleProperties(final String clientId, final String clientSecret, final String oAuthEndPoint,
-                            final String responseType, final List<String> scopes, final String tokenUri,
-                            final String accessType) {
+    public GoogleProperties(@Value("${oauth.google.client-id}") final String clientId,
+                            @Value("${oauth.google.client-secret}") final String clientSecret,
+                            @Value("${oauth.google.oauth-end-point}") final String oAuthEndPoint,
+                            @Value("${oauth.google.response-type}") final String responseType,
+                            @Value("${oauth.google.scopes}") final String scopes,
+                            @Value("${oauth.google.token-uri}") final String tokenUri,
+                            @Value("${oauth.google.access-type}") final String accessType) {
         this.clientId = clientId;
         this.clientSecret = clientSecret;
         this.oAuthEndPoint = oAuthEndPoint;
@@ -44,7 +47,7 @@ public class GoogleProperties {
         return responseType;
     }
 
-    public List<String> getScopes() {
+    public String getScopes() {
         return scopes;
     }
 
