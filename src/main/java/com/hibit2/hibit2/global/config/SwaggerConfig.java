1ddd624e553f6467e9e74a7925b04fb0e8418be1
Junyong Moon <fancy.junyongmoon@gmail.com>
2023-07-31 18:31:45 +0900

    feat: Bearer JWT를 이용한 api 인증하기 위해 추가

--- src/main/java/com/hibit2/hibit2/global/config/SwaggerConfig.java
+++ src/main/java/com/hibit2/hibit2/global/config/SwaggerConfig.java
@@ -1,13 +1,17 @@
 package com.hibit2.hibit2.global.config;
 
-import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Collections;
+import java.util.Arrays;
+
 
 @Configuration
 public class SwaggerConfig {
@@ -20,7 +24,15 @@ public class SwaggerConfig {
     }
     @Bean
     public OpenAPI springShopOpenAPI() {
+
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+        SecurityRequirement schemaRequirement = new SecurityRequirement().addList("bearerAuth");
+
         return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .security(Arrays.asList(schemaRequirement))
                 .info(new Info().title("HIBIT API")
                         .description("Hibit API 명세서입니다.")
                         .version("v0.0.1"));
