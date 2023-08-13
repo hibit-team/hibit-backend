d7a480a74b4a256673bb8b7de0e30b33a4a9fc06
Junyong Moon <fancy.junyongmoon@gmail.com>
2023-08-13 21:32:42 +0900

    feat: 회원 정보에 대한 예시 값 추가

--- src/test/java/com/hibit2/hibit2/common/Constants.java
+++ src/test/java/com/hibit2/hibit2/common/Constants.java
@@ -2,4 +2,8 @@ package com.hibit2.hibit2.common;
 
 public class Constants {
     public static final String 팬시_이메일 = "fancy@email.com";
+
+    public static final String 팬시_이름 = "fancy";
+
+    public static final String 팬시_프로필_URL = "/fancy.png";
 }
