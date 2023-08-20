07fc30323be11e0fd1ceff1474abd41c54cedadf
devfancy <fancy.junyongmoon@gmail.com>
2023-08-20 11:54:58 +0900

    feat: 액세스 토큰을 발급할 때 프로필 여부를 확인하는 코드 추가

--- src/main/java/com/hibit2/hibit2/auth/domain/AuthAccessToken.java
+++ src/main/java/com/hibit2/hibit2/auth/domain/AuthAccessToken.java
@@ -1,16 +1,14 @@
-package com.hibit2.hibit2.auth.dto.response;
+package com.hibit2.hibit2.auth.domain;
 
-public class AccessTokenResponse {
+// 소셜 로그인으로 액세스 토큰 발급과 프로필 여부를 확인하는 클래스
+public class AuthAccessToken {
 
     private Long id;
     private String accessToken;
 
     private int isProfileRegistered;
 
-    private AccessTokenResponse() {
-    }
-
-    public AccessTokenResponse(Long id, String accessToken, int isProfileRegistered) {
+    public AuthAccessToken(final Long id, final String accessToken, final int isProfileRegistered) {
         this.id = id;
         this.accessToken = accessToken;
         this.isProfileRegistered = isProfileRegistered;
